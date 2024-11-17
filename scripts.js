d9d18f1d2597e43a76dc4a9861ad8bc95f57ade6
declanr <132320097+BreadNZ@users.noreply.github.com>
2024-11-18 00:02:50 +1300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,6 @@
+console.log("scripts.js is loaded and running!");
+
+function buyPlan(planName) {
+    console.log(`Buying plan: ${planName}`);
+    alert(`Plan ${planName} bought successfully!`);
+}
